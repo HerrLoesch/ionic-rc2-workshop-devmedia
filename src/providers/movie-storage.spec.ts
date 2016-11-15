36f500155466a8ecb8b2f847b1895a8292d2a122
Hendrik Lösch <loeschhendrik@googlemail.com>
2016-11-15 15:31:42 +0100

    Ressources

--- src/providers/movie-storage.spec.ts
+++ src/providers/movie-storage.spec.ts
@@ -3,15 +3,13 @@ import { MovieStorage } from './movies-storage';
 
 import {  } from '@angular/core/testing';
 import { TestUtils }               from '../test';
- 
-
-describe('movie services', () => { 
 
 
+describe('movie services', () => {
 
 
 
-    it('should return true', () => { 
-        expect(true).toBeTruthy(); 
-    }); 
+    it('should return true', () => {
+        expect(false).toBeTruthy();
+    });
 });
